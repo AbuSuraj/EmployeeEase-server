83bc7dc1d14a7c92b191d060e79b27b683616356
Abu Suraj <abusurajbd@gmail.com>
2024-01-11 12:25:15 +0600

    add db connection string

--- utils/db.connect.js
+++ utils/db.connect.js
@@ -1,18 +1,10 @@
-// import mysql from "mysql"
-
-// export const db = mysql.createConnection({
-//   host:"gateway01.ap-southeast-1.prod.aws.tidbcloud.com",
-//   user:"aWLw16Hy7BmSsP3.root",
-//   password:"yzbY5tYTX8oI1hcy",
-//   database:"social",
-//   CA: './isrgrootx1.pem'
-// })
 import mysql from "mysql2";
 
 // Connection string
-const connectionString = "mysql://aWLw16Hy7BmSsP3.root:yzbY5tYTX8oI1hcy@gateway01.ap-southeast-1.prod.aws.tidbcloud.com:4000/social?ssl={\"rejectUnauthorized\":true}";
+const connectionString = "mysql://43a5mdUeKWoCZAw.root:B2bhgFdXyPrm1qiW@gateway01.ap-southeast-1.prod.aws.tidbcloud.com:4000/test?ssl={\"rejectUnauthorized\":true}";
 
-// const connectionString = "mysql://aWLw16Hy7BmSsP3.root:yzbY5tYTX8oI1hcy@gateway01.ap-southeast-1.prod.aws.tidbcloud.com:4000/social";
+// mysql://43a5mdUeKWoCZAw.root:B2bhgFdXyPrm1qiW@gateway01.ap-southeast-1.prod.aws.tidbcloud.com:4000/test?ssl={"rejectUnauthorized":true}
+ 
 
 // Create a connection pool
 export const db = mysql.createPool(connectionString);
